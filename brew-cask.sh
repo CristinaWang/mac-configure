396c758496dd81d0ec697979b5a3bd78c28ca086
cristina.wang <cristina.wang@xplusz.com>
2017-04-24 10:29:28 +0800

    Init

---
+++ brew-cask.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# to maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
+
+# Not on cask but I want regardless.
+
+# File Multi Tool 5
+# Phosphor
